8b288af852149d9d175305d7d6d3d6524bee6381
NosoaB17 <ngsn176@gmail.com>
2024-08-13 17:40:14 +0700

    Lesson 16

---
+++ src/components/MyComponent.js
@@ -0,0 +1,18 @@
+// class Component
+// function Component
+
+import React from "react";
+
+class MyComponent extends React.Component {
+  // JSX
+  render() {
+    return (
+      <div>
+        My First Component
+        {Math.random()}
+      </div>
+    );
+  }
+}
+
+export default MyComponent;
