ab298fa5379251975a15f935a2bac471e4164cb1
NosoaB17 <ngsn176@gmail.com>
2024-08-14 14:22:58 +0700

    DOM Events

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -2,19 +2,29 @@
 // function Component
 
 import React from "react";
-
 class MyComponent extends React.Component {
   state = {
     name: "SonNB",
     address: "Wuan Nhan",
     age: 23,
   };
+
+  handleClick(event) {
+    console.log("Click my button");
+    console.log("My name is ", this.state.name);
+  }
+
+  handleOnMouseOver(event) {
+    console.log(event.pageX);
+  }
   // JSX
   render() {
     return (
       <div>
         My name is {this.state.name} and i live in {this.state.address}. I'm{" "}
         {this.state.age} years old.
+        <button onClick={this.handleClick}>Click me</button>
+        <button onMouseOver={this.handleOnMouseOver}>Hover me</button>
       </div>
     );
   }
