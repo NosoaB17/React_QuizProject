71ac1a5719c1f1a76b93dcd9cd11aa923c25f3e3
NosoaB17 <ngsn176@gmail.com>
2024-08-19 14:17:43 +0700

    Header

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
-import logo from "./logo.svg";
 import "./App.scss";
-import { useDispatch, useSelector } from "react-redux";
-import { increaseCounter, decreaseCounter } from "./redux/action/counterAction";
-import MyComponent from "./components/MyComponent";
-import React from "react";
-
-// class App extends React.Component {
-//   render() {
-//     return (
-//       <div className="app-container">
-//         Hello NosoaB to React &amp; Ngo Bao Son
-//         <MyComponent></MyComponent>
-//       </div>
-//     );
-//   }
-// }
+import Header from "./components/Header/Header";
 
 const App = () => {
   return (
-    <div className="app-container">
-      Hello NosoaB to React &amp; Ngo Bao Son
-      <MyComponent></MyComponent>
+    <div className="app-conatiner">
+      <Header />
     </div>
   );
 };
