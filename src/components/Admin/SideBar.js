a2e175c3a5e8f74a35184ad5551ee69967e478f5
NosoaB17 <ngsn176@gmail.com>
2024-08-21 18:20:10 +0700

    Complete SideBar

---
+++ src/components/Admin/SideBar.js
@@ -0,0 +1,98 @@
+import "react-pro-sidebar/dist/css/styles.css";
+import { SiReactivex } from "react-icons/si";
+import {
+  ProSidebar,
+  Menu,
+  MenuItem,
+  SubMenu,
+  SidebarHeader,
+  SidebarFooter,
+  SidebarContent,
+} from "react-pro-sidebar";
+
+import {
+  FaTachometerAlt,
+  FaGem,
+  FaList,
+  FaGithub,
+  FaRegLaughWink,
+  FaHeart,
+} from "react-icons/fa";
+
+import sidebarBg from "../../assets/bg2.jpg";
+import "./SideBar.scss";
+
+const SideBar = (props) => {
+  const { image, collapsed, toggled, handleToggleSidebar } = props;
+  return (
+    <>
+      <ProSidebar
+        image={sidebarBg}
+        collapsed={collapsed}
+        toggled={toggled}
+        breakPoint="md"
+        onToggle={handleToggleSidebar}
+      >
+        <SidebarHeader>
+          <div
+            style={{
+              padding: "24px",
+              textTransform: "uppercase",
+              fontWeight: "bold",
+              fontSize: 14,
+              letterSpacing: "1px",
+              overflow: "hidden",
+              textOverflow: "ellipsis",
+              whiteSpace: "nowrap",
+            }}
+          >
+            <SiReactivex size={"4em"} color={"00bfff"} />
+            <span>SonNB</span>
+          </div>
+        </SidebarHeader>
+
+        <SidebarContent>
+          <Menu iconShape="circle">
+            <MenuItem icon={<FaTachometerAlt />}>Dashboard</MenuItem>
+          </Menu>
+          <Menu iconShape="circle">
+            <SubMenu icon={<FaGem />} title="Features">
+              <MenuItem> Users </MenuItem>
+              <MenuItem> Quiz</MenuItem>
+              <MenuItem> Question</MenuItem>
+            </SubMenu>
+          </Menu>
+        </SidebarContent>
+
+        <SidebarFooter style={{ textAlign: "center" }}>
+          <div
+            className="sidebar-btn-wrapper"
+            style={{
+              padding: "20px 24px",
+            }}
+          >
+            <a
+              href="https://github.com/NosoaB17"
+              target="_blank"
+              className="sidebar-btn"
+              rel="noopener noreferrer"
+            >
+              <FaGithub />
+              <span
+                style={{
+                  whiteSpace: "nowrap",
+                  textOverflow: "ellipsis",
+                  overflow: "hidden",
+                }}
+              >
+                NosoaB
+              </span>
+            </a>
+          </div>
+        </SidebarFooter>
+      </ProSidebar>
+    </>
+  );
+};
+
+export default SideBar;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -31,11 +31,11 @@ const Header = () => {
           <Nav>
             <button className="btn-login">Log In</button>
             <button className="btn-signup">Sign Up</button>
-            <NavDropdown title="Setting" id="basic-nav-dropdown">
+            {/* <NavDropdown title="Setting" id="basic-nav-dropdown">
               <NavDropdown.Item>Log in</NavDropdown.Item>
               <NavDropdown.Item>Log out</NavDropdown.Item>
               <NavDropdown.Item>Profile</NavDropdown.Item>
-            </NavDropdown>
+            </NavDropdown> */}
           </Nav>
         </Navbar.Collapse>
       </Container>
